d2eb0f601c1a38f08238f7899dc47b29dbc00bed
Eric Haseltine <eric@choppingblock.com>
2012-09-21 12:54:05 -0400

    And now we use Classes and object for things

--- public/assets/javascript/app.coffee
+++ public/assets/javascript/app.coffee
@@ -3,176 +3,211 @@
  */
 ;window.Modernizr=function(a,b,c){function v(a){i.cssText=a}function w(a,b){return v(l.join(a+";")+(b||""))}function x(a,b){return typeof a===b}function y(a,b){return!!~(""+a).indexOf(b)}function z(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:x(f,"function")?f.bind(d||b):f}return!1}var d="2.5.3",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l=" -webkit- -moz- -o- -ms- ".split(" "),m={},n={},o={},p=[],q=p.slice,r,s=function(a,c,d,e){var h,i,j,k=b.createElement("div"),l=b.body,m=l?l:b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:g+(d+1),k.appendChild(j);return h=["&#173;","<style>",a,"</style>"].join(""),k.id=g,(l?k:m).innerHTML+=h,m.appendChild(k),l||(m.style.background="",f.appendChild(m)),i=c(k,a),l?k.parentNode.removeChild(k):m.parentNode.removeChild(m),!!i},t={}.hasOwnProperty,u;!x(t,"undefined")&&!x(t.call,"undefined")?u=function(a,b){return t.call(a,b)}:u=function(a,b){return b in a&&x(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=q.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(q.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(q.call(arguments)))};return e});var A=function(c,d){var f=c.join(""),g=d.length;s(f,function(c,d){var f=b.styleSheets[b.styleSheets.length-1],h=f?f.cssRules&&f.cssRules[0]?f.cssRules[0].cssText:f.cssText||"":"",i=c.childNodes,j={};while(g--)j[i[g].id]=i[g];e.touch="ontouchstart"in a||a.DocumentTouch&&b instanceof DocumentTouch||(j.touch&&j.touch.offsetTop)===9},g,d)}([,["@media (",l.join("touch-enabled),("),g,")","{#touch{top:9px;position:absolute}}"].join("")],[,"touch"]);m.touch=function(){return e.touch};for(var B in m)u(m,B)&&(r=B.toLowerCase(),e[r]=m[B](),p.push((e[r]?"":"no-")+r));return v(""),h=j=null,e._version=d,e._prefixes=l,e.testStyles=s,e}(this,this.document);`
 
+
+animateSpinner = () ->
+	i = 0
+	frames = ["/", "|", "\\", "-"]
+
+	setInterval(() ->
+		document.getElementById("spinner").innerHTML = frames[i];
+		i = if i<3 then i+1 else 0
+		(i)
+	, 100)
+
 ##Convienence Functions
 
+#Function to set Touch events on appropriate devices
 addTouchClickEventListener = (element, func) ->
 	# If touch, 
 	if Modernizr.touch
 		element.addEventListener("touchend", func, false);
 	else
 		element.addEventListener("click", func, false);
-	
 
-##UI Functions
-updatePageTitle = (up) ->
-	if up
-		setTaskCount(1)
+removeTouchClickEventListener = (element, func) ->
+	# If touch, 	
+	if Modernizr.touch
+		element.removeEventListener("touchend", func, false);
 	else
-		setTaskCount(-1)
-
-	taskcount = getTaskCount()
-	if  taskcount > 1
-		document.title = taskcount + " tasks :: todo.designedbyscience.com"
-		window.fluid?.dockBadge = taskcount + ""
-	else
-		document.title = taskcount + " task :: todo.designedbyscience.com"
-		window.fluid?.dockBadge = taskcount + ""		
-		
-# fluidDockIcon = (num) ->
-
+		element.removeEventListener("click", func, false);
 			
-animateSpinner = () ->
-	i = 0
-	frames = ["/", "|", "\\", "-"]
+class Spinner
+	constructor: () ->
+		@element = document.createElement("div")
+		@element.innerHTML = "~"
+		@element.classList.add("processspinner")
 	
-	setInterval(() ->
-		document.getElementById("spinner").innerHTML = frames[i];
-		i = if i<3 then i+1 else 0
-	, 100)
+	remove: ()->
+		@element.parentElement.removeChild(@element)	
 
-setTaskFocus = () -> 
-	document.querySelector(".task_entry").focus();	
-	
-activeInput	= document.querySelector(".task_entry");
+class TodoApp
+	constructor: () ->
+		@console = document.querySelector(".console")
+		@days = (@buildDay day_element for day_element in document.querySelectorAll(".day"))
 
-handleInputFocus = (e) ->
-	activeInput = e.target
+		document.addEventListener("keyup", @handleEnterKey, false)
 
-addFocusListener = (o) ->
-	o.addEventListener("focus", handleInputFocus, false)
-	
-clearActiveInput = () ->
-	activeInput.value = ""
 
-addProcessingSpinner = (element) ->
-	spinner = document.createElement("div")
-	spinner.innerHTML = "~"
-	spinner.classList.add("processspinner")
-	element.appendChild(spinner)	
-
-removeProcessingSpinner = (element) ->
-	spinner = element.querySelector(".processspinner")
-	element.removeChild(spinner)
-	element.classList.remove("processing")
-
-setDisabled = (tl) -> 
-	tl.classList.add("disabled")
-	#tl.children[0].setAttribute()
-
-setCurrentDay = () ->
-	today = new Date();
-	
-	activeInput = document.querySelectorAll(".task_entry")[today.getDay()]
-	
-	activeInput.focus()
-	
-	tasklists = document.querySelectorAll(".day")	
-	#setTasklistDisabled tasklist for tasklist in .splice(0, today.getDay())
-	for tasklist, i in document.querySelectorAll(".day") when i < today.getDay()
-		console.log(i)
-		setDisabled(tasklist)
+	buildDay: (element) ->
+		new TaskDay(element);
 		
-
-#Task management functions
+	outputToConsole: (message) ->
+		newconsoleline = document.createElement("p");
+		newconsoleline.innerHTML = message;
+		@console.appendChild(newconsoleline);
+		true
 	
-addTask = (object, list) ->
-	outputToConsole("Sending task...")	
-	newtask = list.querySelector(".empty")
-	unless newtask
-		newtask = document.createElement("div")
-		newtask.classList.add("task")
-		list.appendChild(newtask)
-		
-	newtask.classList.add("processing")
-	newtask.classList.remove("empty")		
-	newtask.innerHTML = object.task_text
-	addProcessingSpinner(newtask)
-	clearActiveInput();
-	updatePageTitle(true)
+	setCurrentDay: () ->
+		today = new Date();
+		day_num = today.getDay();
+		@activeInput = document.querySelectorAll(".task_entry")[day_num]
+		@activeInput.focus()
+		day.disable() for day, i in @days when i < day_num;
 	
-	$.ajax({
-		url: "/todo/"
-		type: "POST"
-		data: object
-		success: (data, textStatus, jqXHR) ->
-			json = $.parseJSON(data);
-			outputToConsole("task added")
-			newtask.setAttribute("data-id", json.id);
-			removeProcessingSpinner(newtask)
-	})
 	
-	true
-
-
-completeTask = (task) ->
-	task.classList.add("completed")
-	id = task.getAttribute("data-id")
-	updatePageTitle(false)
-	
-	$.ajax({
-		url: "/todo/#{id}" 
-		type: "POST"
-		success: (data, textStatus, jqXHR) ->
-			outputToConsole("task completed")
-	})
-
-uncompleteTask = (task) ->
-	task.classList.remove("completed")
-	id = task.getAttribute("data-id")
-	$.ajax({
-		url: "/todo/#{id}" 
-		type: "POST"
-		success: (data, textStatus, jqXHR) ->
-			outputToConsole("task uncompleted")
-	})
-
-handleTaskListClick = (e) ->
-	if e.target.classList.contains("task") && !e.target.classList.contains("empty")
-		if e.target.classList.contains("completed")
-			uncompleteTask(e.target)
+	updatePageTitle: (up) ->
+		# setTaskCount and getTaskCount currently live on the page as functions, above this scope
+		if up
+			setTaskCount(1)
 		else
-			completeTask(e.target)	
+			setTaskCount(-1)
 
-addTaskListClickListener = (o) ->
-	addTouchClickEventListener(o, handleTaskListClick)
-	# o.addEventListener("click", handleTaskListClick, false)
+		taskcount = getTaskCount()
+		if  taskcount > 1
+			document.title = taskcount + " tasks :: todo.designedbyscience.com"
+			window.fluid?.dockBadge = taskcount + ""
+		else
+			document.title = taskcount + " task :: todo.designedbyscience.com"
+			window.fluid?.dockBadge = taskcount + ""
 
-handleEnterKey = (e) ->
-	if e.keyCode is 13
-		# find active task entry
-		task = activeInput.value
-		task_date = activeInput.getAttribute("data-date");
-		task_object = {task_text: task, due_date: task_date}
-	    # send to server
-		addTask(task_object, activeInput.parentNode)
+	handleEnterKey: (e) ->
+		if e.keyCode is 13
+			# find active task entry
+			task = app.activeInput.value
+			
+			task_date = app.activeInput.getAttribute("data-date");
+			task_object = {task_text: task, due_date: task_date}
+		    # send to server
+			app.activeInput.object.addTask(task_object)
 
-myconsole = document.querySelector(".console");
-outputToConsole = (message) ->
-	newconsoleline = document.createElement("p");
-	newconsoleline.innerHTML = message;
-	myconsole.appendChild(newconsoleline);
-	console.log(message)
-	true
+
+
+
+
+class TaskDay
+	constructor: (@element) ->
+		@tasklist = new TaskList(@element.querySelector(".tasklist"));
+
+	disable: () -> 
+		@element.classList.add("disabled")
+		removeTouchClickEventListener(@tasklist.element,@tasklist.handleClick);
+
+class TaskList
+	constructor: (@element) ->
+		@tasks = @buildTasks();
+		@input = @element.querySelector("input");
+		@input.object = @
+
+		addTouchClickEventListener(@element, @handleClick)
+		@input.addEventListener("focus", @handleFocus, false);
+		
+	handleFocus: (e) ->
+		app.activeInput = this;				
+		
+	buildTasks: () ->
+		tasks = [];
+		tasks = (new Task(el) for el in @element.querySelectorAll(".task") when not el.classList.contains("empty"))
+		
+	handleClick: (e) ->
+		if e.target.classList.contains("task") && !e.target.classList.contains("empty")
+			#Find task
+			task = e.target.object
+			task.toggle();
+
+	findTask: (id) ->
+		(task for task in @tasks when task.id is id)[0]
+
+
+	addTask: (object) ->
+		app.outputToConsole("Sending task...")	
+		new_task_element = @element.querySelector(".empty")
+		unless new_task_element
+			new_task_element = Task.buildElement()
+			@element.appendChild(new_task_element)
+
+		task = new Task(new_task_element)
+		task.build(object.tast_text)
+
+		spinner = new Spinner();
+
+		task.element.appendChild(spinner.element);
+		@input.value = "";
+		app.updatePageTitle(true)
+
+		$.ajax({
+			url: "/todo/"
+			type: "POST"
+			data: object
+			success: (data, textStatus, jqXHR) ->
+				json = $.parseJSON(data);
+				app.outputToConsole("task added")
+				task.set_id(json.id)
+				spinner.remove()
+		})
+
+		true
+			
+class Task
 	
+	@buildElement: () ->
+		element = document.createElement("div")
+		element.classList.add("task")
+		(element)
+		
+	constructor: (@element) ->
+		@element.object = @
+		@completed = @element.classList.contains("completed")
+		@id = @element.getAttribute("data-id");
+	
+	build: (text) ->
+		@element.classList.add("processing")
+		@element.classList.remove("empty")		
+		@element.innerHTML = text
+		
+	set_id: (id) ->
+		@element.setAttribute("data-id", json.id);
+		@id = json.id;		
+	
+	toggle: () ->
+		if @completed
+			@uncomplete()
+		else
+			@complete()
+			
+		@completed = @element.classList.contains("completed")			
+
+	complete: () ->
+		@element.classList.add("completed")
+		app.updatePageTitle(false);
+
+		$.ajax({
+			url: "/todo/#{@id}" 
+			type: "POST"
+			success: (data, textStatus, jqXHR) ->
+				app.outputToConsole("task completed")
+		})
+	
+	uncomplete: () ->
+		@element.classList.remove("completed")
+		app.updatePageTitle(true);
+					
+		$.ajax({
+			url: "/todo/#{@id}" 
+			type: "POST"
+			success: (data, textStatus, jqXHR) ->
+				app.outputToConsole("task uncompleted")
+		})
 	
 ##Page Load Items	
-
-#animateSpinner();
-setCurrentDay();
-
-document.addEventListener("keyup", handleEnterKey, false)
-
-addFocusListener input for input in document.querySelectorAll(".task_entry")
-addTaskListClickListener input for input in document.querySelectorAll(".tasklist")
-	
-	
+app = new TodoApp();
+app.setCurrentDay();
--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -5,7 +5,18 @@
  */
 ;window.Modernizr=function(a,b,c){function v(a){i.cssText=a}function w(a,b){return v(l.join(a+";")+(b||""))}function x(a,b){return typeof a===b}function y(a,b){return!!~(""+a).indexOf(b)}function z(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:x(f,"function")?f.bind(d||b):f}return!1}var d="2.5.3",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l=" -webkit- -moz- -o- -ms- ".split(" "),m={},n={},o={},p=[],q=p.slice,r,s=function(a,c,d,e){var h,i,j,k=b.createElement("div"),l=b.body,m=l?l:b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:g+(d+1),k.appendChild(j);return h=["&#173;","<style>",a,"</style>"].join(""),k.id=g,(l?k:m).innerHTML+=h,m.appendChild(k),l||(m.style.background="",f.appendChild(m)),i=c(k,a),l?k.parentNode.removeChild(k):m.parentNode.removeChild(m),!!i},t={}.hasOwnProperty,u;!x(t,"undefined")&&!x(t.call,"undefined")?u=function(a,b){return t.call(a,b)}:u=function(a,b){return b in a&&x(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=q.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(q.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(q.call(arguments)))};return e});var A=function(c,d){var f=c.join(""),g=d.length;s(f,function(c,d){var f=b.styleSheets[b.styleSheets.length-1],h=f?f.cssRules&&f.cssRules[0]?f.cssRules[0].cssText:f.cssText||"":"",i=c.childNodes,j={};while(g--)j[i[g].id]=i[g];e.touch="ontouchstart"in a||a.DocumentTouch&&b instanceof DocumentTouch||(j.touch&&j.touch.offsetTop)===9},g,d)}([,["@media (",l.join("touch-enabled),("),g,")","{#touch{top:9px;position:absolute}}"].join("")],[,"touch"]);m.touch=function(){return e.touch};for(var B in m)u(m,B)&&(r=B.toLowerCase(),e[r]=m[B](),p.push((e[r]?"":"no-")+r));return v(""),h=j=null,e._version=d,e._prefixes=l,e.testStyles=s,e}(this,this.document);;
 
-  var activeInput, addFocusListener, addProcessingSpinner, addTask, addTaskListClickListener, addTouchClickEventListener, animateSpinner, clearActiveInput, completeTask, handleEnterKey, handleInputFocus, handleTaskListClick, input, myconsole, outputToConsole, removeProcessingSpinner, setCurrentDay, setDisabled, setTaskFocus, uncompleteTask, updatePageTitle, _i, _j, _len, _len1, _ref, _ref1;
+  var Spinner, Task, TaskDay, TaskList, TodoApp, addTouchClickEventListener, animateSpinner, app, removeTouchClickEventListener;
+
+  animateSpinner = function() {
+    var frames, i;
+    i = 0;
+    frames = ["/", "|", "\\", "-"];
+    return setInterval(function() {
+      document.getElementById("spinner").innerHTML = frames[i];
+      i = i < 3 ? i + 1 : 0;
+      return i;
+    }, 100);
+  };
 
   addTouchClickEventListener = function(element, func) {
     if (Modernizr.touch) {
@@ -15,198 +26,282 @@
     }
   };
 
-  updatePageTitle = function(up) {
-    var taskcount, _ref, _ref1;
-    if (up) {
-      setTaskCount(1);
+  removeTouchClickEventListener = function(element, func) {
+    if (Modernizr.touch) {
+      return element.removeEventListener("touchend", func, false);
     } else {
-      setTaskCount(-1);
-    }
-    taskcount = getTaskCount();
-    if (taskcount > 1) {
-      document.title = taskcount + " tasks :: todo.designedbyscience.com";
-      return (_ref = window.fluid) != null ? _ref.dockBadge = taskcount + "" : void 0;
-    } else {
-      document.title = taskcount + " task :: todo.designedbyscience.com";
-      return (_ref1 = window.fluid) != null ? _ref1.dockBadge = taskcount + "" : void 0;
+      return element.removeEventListener("click", func, false);
     }
   };
 
-  animateSpinner = function() {
-    var frames, i;
-    i = 0;
-    frames = ["/", "|", "\\", "-"];
-    return setInterval(function() {
-      document.getElementById("spinner").innerHTML = frames[i];
-      return i = i < 3 ? i + 1 : 0;
-    }, 100);
-  };
+  Spinner = (function() {
 
-  setTaskFocus = function() {
-    return document.querySelector(".task_entry").focus();
-  };
-
-  activeInput = document.querySelector(".task_entry");
-
-  handleInputFocus = function(e) {
-    return activeInput = e.target;
-  };
-
-  addFocusListener = function(o) {
-    return o.addEventListener("focus", handleInputFocus, false);
-  };
-
-  clearActiveInput = function() {
-    return activeInput.value = "";
-  };
-
-  addProcessingSpinner = function(element) {
-    var spinner;
-    spinner = document.createElement("div");
-    spinner.innerHTML = "~";
-    spinner.classList.add("processspinner");
-    return element.appendChild(spinner);
-  };
-
-  removeProcessingSpinner = function(element) {
-    var spinner;
-    spinner = element.querySelector(".processspinner");
-    element.removeChild(spinner);
-    return element.classList.remove("processing");
-  };
-
-  setDisabled = function(tl) {
-    return tl.classList.add("disabled");
-  };
-
-  setCurrentDay = function() {
-    var i, tasklist, tasklists, today, _i, _len, _ref, _results;
-    today = new Date();
-    activeInput = document.querySelectorAll(".task_entry")[today.getDay()];
-    activeInput.focus();
-    tasklists = document.querySelectorAll(".day");
-    _ref = document.querySelectorAll(".day");
-    _results = [];
-    for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
-      tasklist = _ref[i];
-      if (!(i < today.getDay())) {
-        continue;
-      }
-      console.log(i);
-      _results.push(setDisabled(tasklist));
+    function Spinner() {
+      this.element = document.createElement("div");
+      this.element.innerHTML = "~";
+      this.element.classList.add("processspinner");
     }
-    return _results;
-  };
 
-  addTask = function(object, list) {
-    var newtask;
-    outputToConsole("Sending task...");
-    newtask = list.querySelector(".empty");
-    if (!newtask) {
-      newtask = document.createElement("div");
-      newtask.classList.add("task");
-      list.appendChild(newtask);
+    Spinner.prototype.remove = function() {
+      return this.element.parentElement.removeChild(this.element);
+    };
+
+    return Spinner;
+
+  })();
+
+  TodoApp = (function() {
+
+    function TodoApp() {
+      var day_element;
+      this.console = document.querySelector(".console");
+      this.days = (function() {
+        var _i, _len, _ref, _results;
+        _ref = document.querySelectorAll(".day");
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          day_element = _ref[_i];
+          _results.push(this.buildDay(day_element));
+        }
+        return _results;
+      }).call(this);
+      document.addEventListener("keyup", this.handleEnterKey, false);
     }
-    newtask.classList.add("processing");
-    newtask.classList.remove("empty");
-    newtask.innerHTML = object.task_text;
-    addProcessingSpinner(newtask);
-    clearActiveInput();
-    updatePageTitle(true);
-    $.ajax({
-      url: "/todo/",
-      type: "POST",
-      data: object,
-      success: function(data, textStatus, jqXHR) {
-        var json;
-        json = $.parseJSON(data);
-        outputToConsole("task added");
-        newtask.setAttribute("data-id", json.id);
-        return removeProcessingSpinner(newtask);
-      }
-    });
-    return true;
-  };
 
-  completeTask = function(task) {
-    var id;
-    task.classList.add("completed");
-    id = task.getAttribute("data-id");
-    updatePageTitle(false);
-    return $.ajax({
-      url: "/todo/" + id,
-      type: "POST",
-      success: function(data, textStatus, jqXHR) {
-        return outputToConsole("task completed");
-      }
-    });
-  };
+    TodoApp.prototype.buildDay = function(element) {
+      return new TaskDay(element);
+    };
 
-  uncompleteTask = function(task) {
-    var id;
-    task.classList.remove("completed");
-    id = task.getAttribute("data-id");
-    return $.ajax({
-      url: "/todo/" + id,
-      type: "POST",
-      success: function(data, textStatus, jqXHR) {
-        return outputToConsole("task uncompleted");
-      }
-    });
-  };
+    TodoApp.prototype.outputToConsole = function(message) {
+      var newconsoleline;
+      newconsoleline = document.createElement("p");
+      newconsoleline.innerHTML = message;
+      this.console.appendChild(newconsoleline);
+      return true;
+    };
 
-  handleTaskListClick = function(e) {
-    if (e.target.classList.contains("task") && !e.target.classList.contains("empty")) {
-      if (e.target.classList.contains("completed")) {
-        return uncompleteTask(e.target);
+    TodoApp.prototype.setCurrentDay = function() {
+      var day, day_num, i, today, _i, _len, _ref, _results;
+      today = new Date();
+      day_num = today.getDay();
+      this.activeInput = document.querySelectorAll(".task_entry")[day_num];
+      this.activeInput.focus();
+      _ref = this.days;
+      _results = [];
+      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+        day = _ref[i];
+        if (i < day_num) {
+          _results.push(day.disable());
+        }
+      }
+      return _results;
+    };
+
+    TodoApp.prototype.updatePageTitle = function(up) {
+      var taskcount, _ref, _ref1;
+      if (up) {
+        setTaskCount(1);
       } else {
-        return completeTask(e.target);
+        setTaskCount(-1);
       }
+      taskcount = getTaskCount();
+      if (taskcount > 1) {
+        document.title = taskcount + " tasks :: todo.designedbyscience.com";
+        return (_ref = window.fluid) != null ? _ref.dockBadge = taskcount + "" : void 0;
+      } else {
+        document.title = taskcount + " task :: todo.designedbyscience.com";
+        return (_ref1 = window.fluid) != null ? _ref1.dockBadge = taskcount + "" : void 0;
+      }
+    };
+
+    TodoApp.prototype.handleEnterKey = function(e) {
+      var task, task_date, task_object;
+      if (e.keyCode === 13) {
+        task = app.activeInput.value;
+        task_date = app.activeInput.getAttribute("data-date");
+        task_object = {
+          task_text: task,
+          due_date: task_date
+        };
+        return app.activeInput.object.addTask(task_object);
+      }
+    };
+
+    return TodoApp;
+
+  })();
+
+  TaskDay = (function() {
+
+    function TaskDay(element) {
+      this.element = element;
+      this.tasklist = new TaskList(this.element.querySelector(".tasklist"));
     }
-  };
 
-  addTaskListClickListener = function(o) {
-    return addTouchClickEventListener(o, handleTaskListClick);
-  };
+    TaskDay.prototype.disable = function() {
+      this.element.classList.add("disabled");
+      return removeTouchClickEventListener(this.tasklist.element, this.tasklist.handleClick);
+    };
 
-  handleEnterKey = function(e) {
-    var task, task_date, task_object;
-    if (e.keyCode === 13) {
-      task = activeInput.value;
-      task_date = activeInput.getAttribute("data-date");
-      task_object = {
-        task_text: task,
-        due_date: task_date
-      };
-      return addTask(task_object, activeInput.parentNode);
+    return TaskDay;
+
+  })();
+
+  TaskList = (function() {
+
+    function TaskList(element) {
+      this.element = element;
+      this.tasks = this.buildTasks();
+      this.input = this.element.querySelector("input");
+      this.input.object = this;
+      addTouchClickEventListener(this.element, this.handleClick);
+      this.input.addEventListener("focus", this.handleFocus, false);
     }
-  };
 
-  myconsole = document.querySelector(".console");
+    TaskList.prototype.handleFocus = function(e) {
+      return app.activeInput = this;
+    };
 
-  outputToConsole = function(message) {
-    var newconsoleline;
-    newconsoleline = document.createElement("p");
-    newconsoleline.innerHTML = message;
-    myconsole.appendChild(newconsoleline);
-    console.log(message);
-    return true;
-  };
+    TaskList.prototype.buildTasks = function() {
+      var el, tasks;
+      tasks = [];
+      return tasks = (function() {
+        var _i, _len, _ref, _results;
+        _ref = this.element.querySelectorAll(".task");
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          el = _ref[_i];
+          if (!el.classList.contains("empty")) {
+            _results.push(new Task(el));
+          }
+        }
+        return _results;
+      }).call(this);
+    };
 
-  setCurrentDay();
+    TaskList.prototype.handleClick = function(e) {
+      var task;
+      if (e.target.classList.contains("task") && !e.target.classList.contains("empty")) {
+        task = e.target.object;
+        return task.toggle();
+      }
+    };
 
-  document.addEventListener("keyup", handleEnterKey, false);
+    TaskList.prototype.findTask = function(id) {
+      var task;
+      return ((function() {
+        var _i, _len, _ref, _results;
+        _ref = this.tasks;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          task = _ref[_i];
+          if (task.id === id) {
+            _results.push(task);
+          }
+        }
+        return _results;
+      }).call(this))[0];
+    };
 
-  _ref = document.querySelectorAll(".task_entry");
-  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-    input = _ref[_i];
-    addFocusListener(input);
-  }
+    TaskList.prototype.addTask = function(object) {
+      var new_task_element, spinner, task;
+      app.outputToConsole("Sending task...");
+      new_task_element = this.element.querySelector(".empty");
+      if (!new_task_element) {
+        new_task_element = Task.buildElement();
+        this.element.appendChild(new_task_element);
+      }
+      task = new Task(new_task_element);
+      task.build(object.tast_text);
+      spinner = new Spinner();
+      task.element.appendChild(spinner.element);
+      this.input.value = "";
+      app.updatePageTitle(true);
+      $.ajax({
+        url: "/todo/",
+        type: "POST",
+        data: object,
+        success: function(data, textStatus, jqXHR) {
+          var json;
+          json = $.parseJSON(data);
+          app.outputToConsole("task added");
+          task.set_id(json.id);
+          return spinner.remove();
+        }
+      });
+      return true;
+    };
 
-  _ref1 = document.querySelectorAll(".tasklist");
-  for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-    input = _ref1[_j];
-    addTaskListClickListener(input);
-  }
+    return TaskList;
+
+  })();
+
+  Task = (function() {
+
+    Task.buildElement = function() {
+      var element;
+      element = document.createElement("div");
+      element.classList.add("task");
+      return element;
+    };
+
+    function Task(element) {
+      this.element = element;
+      this.element.object = this;
+      this.completed = this.element.classList.contains("completed");
+      this.id = this.element.getAttribute("data-id");
+    }
+
+    Task.prototype.build = function(text) {
+      this.element.classList.add("processing");
+      this.element.classList.remove("empty");
+      return this.element.innerHTML = text;
+    };
+
+    Task.prototype.set_id = function(id) {
+      this.element.setAttribute("data-id", json.id);
+      return this.id = json.id;
+    };
+
+    Task.prototype.toggle = function() {
+      if (this.completed) {
+        this.uncomplete();
+      } else {
+        this.complete();
+      }
+      return this.completed = this.element.classList.contains("completed");
+    };
+
+    Task.prototype.complete = function() {
+      this.element.classList.add("completed");
+      app.updatePageTitle(false);
+      return $.ajax({
+        url: "/todo/" + this.id,
+        type: "POST",
+        success: function(data, textStatus, jqXHR) {
+          return app.outputToConsole("task completed");
+        }
+      });
+    };
+
+    Task.prototype.uncomplete = function() {
+      this.element.classList.remove("completed");
+      app.updatePageTitle(true);
+      return $.ajax({
+        url: "/todo/" + this.id,
+        type: "POST",
+        success: function(data, textStatus, jqXHR) {
+          return app.outputToConsole("task uncompleted");
+        }
+      });
+    };
+
+    return Task;
+
+  })();
+
+  app = new TodoApp();
+
+  app.setCurrentDay();
 
 }).call(this);
